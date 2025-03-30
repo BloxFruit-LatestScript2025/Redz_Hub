19773c40cc11af302145e64fc17bd35ba2a40347
BloxFruit-LatestScript2025 <thapa43929@gmail.com>
2025-03-30 15:41:43 +0545

    Create Redz.lua

---
+++ Redz.lua
@@ -0,0 +1,9 @@
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/Script2/refs/heads/main/Source"))()
+```
+
+--------
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/Script2/refs/heads/main/Source"))()
+
+For Mobile Copy & Paste ↑↑↑
